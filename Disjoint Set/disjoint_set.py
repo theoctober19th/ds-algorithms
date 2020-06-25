be141a8df3433aa58f10fa61ca9d82e5fe607265
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-06-25 19:09:04 +0545

    disjoint set implemented

---
+++ Disjoint Set/disjoint_set.py
@@ -0,0 +1,32 @@
+class DisjointSet:
+    def __init__(self, nodes):
+        self.parent = {node: node for node in nodes}
+        self.size = {node: 1 for node in nodes}
+        self.num_sets = len(nodes)
+
+    def find(self, node):
+        while self.parent[node] != node:
+            node = self.parent[node]
+        return node
+
+    def union(self, node1, node2):
+        node1 = self.find(node1)
+        node2 = self.find(node2)
+        if self.size[node1] < self.size[node2]:
+            self.parent[node1] = node2
+            self.size[node1] = self.size[node1] + self.size[node2]
+        else:
+            self.parent[node2] = node1
+            self.size[node2] = self.size[node1] + self.size[node2]
+        self.num_sets = self.num_sets - 1
+
+
+def main():
+    ds = DisjointSet(['A', 'B', 'C', 'D', 'E'])
+    ds.union('C', 'A')
+    ds.union('C', 'D')
+    print(ds.find('D'))
+
+
+if __name__ == "__main__":
+    main()
