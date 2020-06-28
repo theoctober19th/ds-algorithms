77917c628b38d903fb07b5c66cfb091cddc3043c
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-06-28 11:07:27 +0545

    setup for algorithms package

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 from setuptools import setup, find_packages
 
 setup(name='data_structures', version='1.0', packages=find_packages())
+setup(name='algorithms', version='1.0', packages=find_packages())
