d1b132bb2c2d0f58292c79e6179afc67c7760299
Bikalpa Dhakal <theoctober19th@gmail.com>
2020-07-02 10:11:28 +0545

    edited setup.py to include examples

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
 
+# for installing data_structures, examples and algorithms directory as packages
 setup(name='data_structures', version='1.0', packages=find_packages())
 setup(name='algorithms', version='1.0', packages=find_packages())
+setup(name='examples', version='1.0', packages=find_packages())
